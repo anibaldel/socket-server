5096c11652a93ec90dea77729918172e0b263f0b
anibaldel <anibaldel2669@gmail.com>
2019-08-24 19:35:07 -0400

    Fin seccion 5

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,16 +1,26 @@
 import { Socket } from 'socket.io';
 import socketIO from 'socket.io';
+import { UsuariosLista } from '../classes/usuarios-lista';
+import { Usuario } from '../classes/usuario';
 
 
 
+export const usuariosConectados = new UsuariosLista();
 
+export const conectarCliente = ( cliente: Socket ) => {
 
+    const usuario = new Usuario( cliente.id );
+    usuariosConectados.agregar( usuario );
+
+}
 
 export const desconectar = ( cliente: Socket ) => {
 
     cliente.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuariosConectados.borrarUsuario( cliente.id );
     });
+
 
 }
 
@@ -28,3 +38,17 @@ export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
 
 }
 
+// Configurar usuario
+export const configurarUsuario = ( cliente: Socket, io: socketIO.Server ) => {
+
+    cliente.on('configurar-usuario', (  payload: { nombre: string }, callback: Function  ) => {
+
+        usuariosConectados.actualizarNombre( cliente.id, payload.nombre );
+
+        callback({
+            ok: true,
+            mensaje: `Usuario ${ payload.nombre }, configurado`
+        });
+    });
+
+}
